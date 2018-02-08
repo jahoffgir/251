c70812882dc6cead9bf9b8f6991ce2ed59d50822
Jahongir A <jxa7578@g.rit.edu>
2018-02-08 12:13:47 -0500

    Commented some lines of code

--- proj1/Quadratic.java
+++ proj1/Quadratic.java
@@ -11,6 +11,7 @@
  */
 public class Quadratic {
     public static void main(String [] args) {
+        // Checking the number of args
         if (args.length != 4) {
             System.err.println("Incorrect number of arguments. Need four arguments.");
             System.exit(1);
@@ -21,18 +22,18 @@ public class Quadratic {
         int max = 0;
         try {
             // coefficient of a, b, c, and max
-            a = Integer.parseInt(args[1]);
-            b = Integer.parseInt(args[2]);
-            c = Integer.parseInt(args[3]);
-            max = Integer.parseInt(args[4]);
+            a = Integer.parseInt(args[0]);
+            b = Integer.parseInt(args[1]);
+            c = Integer.parseInt(args[2]);
+            max = Integer.parseInt(args[3]);
             
         }
-        catch (Exception e)
-        {
+        // Checking the type of the args
+        catch (IllegalArgumentException e) {
             System.err.println("Arguments are not integers. Exiting.");
             System.exit(1);
         }
-
+        // Checkking the value of max
         if (max < 0) {
             System.err.println("Maximum value of x is NOT greater than or equal to 0.");
             System.exit(1);
