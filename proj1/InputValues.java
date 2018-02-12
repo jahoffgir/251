b56730edefde4d899928738ed9f25173961b837a
Jahongir A <jxa7578@g.rit.edu>
2018-02-12 17:53:17 -0500

    Finished implementing the InputValues class

--- proj1/InputValues.java
+++ proj1/InputValues.java
@@ -1,22 +1,35 @@
 /**
  * A thread class that inputs the initial values derived from the command line 
- * into the monitor. There is one instance of this class.
+ *      into the monitor. A runnable object that puts numbers into a HeldValue x 
+ *      times.
  * 
  * @author Jahongir Amirkulov
  * @version 02/12/18
  */
 public class InputValues implements Runnable{
 
+    private int a, b, c;
+    private HeldValue hv;
+
     /**
      * Default Constructor
      */
-    public InputValues() {
-
+    public InputValues(int a, int b, int c, HeldValue hv) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.hv = hv;
     }
 
-    
+    public void run() {
+        try {
+            hv.putF(0, c);
+            //adding a + b and then passing it to putG
+            hv.putG(0, (a + b));
 
-
+            hv.putH(2 * a);
+        } catch (InterruptedException exc) {}   
+    }
 
 
 }
