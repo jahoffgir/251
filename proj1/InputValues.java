022368d0350939116a78975a318f3aacd9297fbe
Jahongir A <jxa7578@g.rit.edu>
2018-02-12 11:17:36 -0500

    Added more new stubbed files

---
+++ proj1/InputValues.java
@@ -0,0 +1,20 @@
+/**
+ * A thread class that inputs the initial values derived from the command line 
+ * into the monitor. There is one instance of this class.
+ * 
+ * @author Jahongir Amirkulov
+ * @version 02/12/18
+ */
+public class InputValues {
+
+    /**
+     * Default Constructor
+     */
+    public InputValues() {
+
+    }
+
+    
+
+
+}
---
+++ proj1/PrintOutput.java
@@ -0,0 +1,16 @@
+/**
+ * A thread class that prints the program's output. There is one instance of 
+ *      this class.
+ * 
+ * @author Jahongit Amirkulov
+ * @version 02/12/18
+ */
+public class PrintOutput {
+    
+    /**
+     * Default Constructor 
+     */
+    public PrintOutput() {
+
+    }
+}
