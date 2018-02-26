f51786776cff3f7c25ee9d02467b9f078e6f5865
Jahongir Amirkulov <jahongiramirkulov@Jahongirs-MBP.wireless.rit.edu>
2018-02-26 18:08:42 -0500

    Added comments and started the project:

---
+++ proj2/Goldbach.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * A Goldbach number is an even number that is the sum of two primes. The
+ * Goldbach Conjecture, posed by German mathematician Christian Goldbach in 1742
+ * and as yet not proven or disproven, is that every even number greater than 4 
+ * is a Goldbach number.
+ *
+ * @author Jahongir Amirkulov
+ * @version 02/26/18
+ *
+ */
+
+public class Goldbach {
+    public static void main(String [] args) {
+        // TODO
+    }
+}
+
