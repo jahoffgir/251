f066353909b3186801bb1fd30503ca747dcdd573
Jahongir Amirkulov <amjahongir@gmail.com>
2018-02-26 20:47:30 -0500

    Added all the edge cases

--- proj2/Goldbach.java
+++ proj2/Goldbach.java
@@ -20,11 +20,42 @@ public class Goldbach {
     public static void main(String [] args) throws Exception {
         // Validate command line arguments.
         if (args.length < 1) {
-            System.error.println("<n> must be an even number > 4");
+            System.err.println("Incorrect number of arguements");
+            System.exit(1);
         }
-        try {
 
-        } catch(Exception)
+        try {
+            // even number n
+            int n = Integer.parseInt(args[0]);
+
+            // checking for even number
+            if (n % 2 != 0) { 
+                System.err.println("<n> must be an even number > 4");
+                System.exit(1);
+            }
+
+            parallelFor (0, n - 1) .exec (new Loop() {
+                /**
+                 * Start method
+                 */
+                public void start() {
+
+                }
+
+                /**
+                 * Run method
+                 * @param i 
+                 */
+                public void run (int i) {
+                    
+                }
+            });
+        } 
+        // Checking the type of the args
+        catch (IllegalArgumentException e) {
+            System.err.println("Argument is not an int. Exiting.");
+            System.exit(1);
+        }
     }
 }
 
